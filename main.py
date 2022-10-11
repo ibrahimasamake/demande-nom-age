11c45d26bbfeaa5b0a5ca76a0da8e3af4fa96cdc
ibrahimasamake <ibrahimasamake1717@gmail.com>
2022-10-11 17:38:39 +0200

    demande nom prenom

---
+++ main.py
@@ -0,0 +1,20 @@
+# demande nom et prenom
+
+def inputUserInfos():   #function
+    while True:
+        nom = input("Entrer votre nom : ")
+        if len(nom) <2 :
+            return inputUserInfos()
+
+        age = input("entre votrer age : ")
+        try:
+            ageInt = int(age)
+        except ValueError:
+            print("vous devez entre un nombre")
+            age = input("entre votrer age")
+        break
+    info = (f"Bonjour,{nom}, tu as {age} ans et bienvenue à ODC.")
+    return info
+
+nameAndAge = inputUserInfos()
+print(nameAndAge)
